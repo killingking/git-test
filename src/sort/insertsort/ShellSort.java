bcfb4b57f1e45bab69bc4ed30922aa2d7607ca05
cyt <1358582642@qq.com>
2021-09-23 15:19:11 +0800

    同一行newbranch提交sout

--- src/sort/insertsort/ShellSort.java
+++ src/sort/insertsort/ShellSort.java
@@ -8,9 +8,13 @@ public class ShellSort {
         //排序
         //arr[index]  和 arr[index+(n/2+1)]
         //第一层循环,进行分组,结束条件就是分组为0
-        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-            for (int i = 0; i < arr.length; i++) {
-
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {//步长分组
+            //第二层循环 按步长插入位置
+            //如何插入
+            //1.根据步长
+            //
+            for (int i = 0; i < arr.length; i+=gap/*记录需要修改的下标的变量*/) {
+                System.out.println("这是newBranch要提交的");
             }
         }
 
